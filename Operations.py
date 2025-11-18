1fb80613037dfcec33b5a0cdda5724e25c988671
Akanksha Jha <akanchhajha96@gmail.com>
2025-11-18 19:03:42 +0530

    Brandwise Product count

--- Operations.py
+++ Operations.py
@@ -24,3 +24,23 @@ with open('C:/Python Course/CSVFile/my-first-git-project/OperationsOnCSV/product
     temp_set['availability'] = lines['Availability']
     temp_set['id'] = lines['Internal ID']
     Products.append(temp_set)
+
+#To calulate Brandwise product count
+cc=0
+cc=Counter(product['brand'] for product in Products)
+print("Number of products as per Brand:")
+with open('brandwise_Productcount.csv', mode ='w')as file:
+    csvFileWriter = csv.writer(file)
+    csvFileWriter.writerow(['Brand', 'Total'])
+    for b, count in cc.items():
+        print(f"{b}: {count}")
+        csvFileWriter.writerow([b,count])
+
+max_brand = ""
+max_count = 0
+
+for brand, count in cc.items():
+    if count > max_count:
+        max_count = count
+        max_brand = brand
+print(f"Brand with highest products: {max_brand} ({max_count} products)")
